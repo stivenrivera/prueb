7f3f033788be262be24a77b58dde84e8c388ae3a
Stiven Rivera <115115438+stivenrivera@users.noreply.github.com>
2023-06-19 09:29:10 -0500

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+
+
+funcionPredeterminada = () => {
+    const formulario = document.getElementById('formulario')
+    formulario.addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        const valorporHora = document.getElementById('valorPorHora');
+        
+        const disenoInterfaz= document.getElementById('interfazHoras');
+        
+        const HorasHTML= document.getElementById('interfazHTML');
+        
+        
+        
+      })
+};
+funcionPredeterminada();
+
+
